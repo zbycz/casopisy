890f32108a5f86ca0910ee586537bb580dd1d472
zbycz <zbytovsky@gmail.com>
2013-11-03 16:00:36 +0100

    Oprava convert-all.sh pro hashované img's.

--- data/convert-all.sh
+++ data/convert-all.sh
@@ -2,7 +2,16 @@
 
 for file in pdf/*.pdf; do 
 	n=`basename $file .pdf`
-	if [ ! -f img/$n-1.png ]; then
+
+	if [ $n -le $1 ]; then
+		continue;
+	fi;
+
+	# potrebuji wildcard - kvuli hashum (predtim: if [ ! -f img/$n-1.png ]; then)
+	ls -U img/$n-* >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo File $file ok.
+	else
 		echo Converting $file...
 		nice -n19 ionice -c3 convert -scene 1 -trim -density 100 $file img/$n.png
 	fi;
