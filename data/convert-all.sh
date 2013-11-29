d8a91bca4a21b9f3ae92af7ab61f218edf580834
zbycz <zbytovsky@gmail.com>
2013-11-29 10:55:49 +0100

    Convert-all také tvoří bookmarks

--- data/convert-all.sh
+++ data/convert-all.sh
@@ -10,5 +10,6 @@ for file in pdf/*.pdf; do
 	if [ ! -f img/$n-1.png ]; then
 		echo Converting $file...
 		nice -n19 ionice -c3 convert -scene 1 -trim -density 100 $file img/$n.png
+		pdftk $file dump_data > $file.bookmarks
 	fi;
 done;
